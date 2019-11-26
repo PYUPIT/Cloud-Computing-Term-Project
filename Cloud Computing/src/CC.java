d94b0c0ec89404ee1a66de6e592ff70dc059d79f
JUNO <JUNO@DESKTOP-OQVL0LD>
2019-11-26 22:06:02 +0900

    - 기능3 ( 인스턴스 시작 ) 추가

--- Cloud Computing/src/CC.java
+++ Cloud Computing/src/CC.java
@@ -4,12 +4,28 @@ import com.amazonaws.AmazonClientException;
 import com.amazonaws.auth.profile.ProfileCredentialsProvider;
 import com.amazonaws.services.ec2.AmazonEC2;
 import com.amazonaws.services.ec2.AmazonEC2ClientBuilder;
+
 import com.amazonaws.services.ec2.model.DescribeInstancesRequest;
 import com.amazonaws.services.ec2.model.DescribeInstancesResult;
 import com.amazonaws.services.ec2.model.Instance;
+
+import com.amazonaws.services.ec2.model.InstanceNetworkInterfaceSpecification;
+
+import com.amazonaws.services.ec2.model.RebootInstancesRequest;
 import com.amazonaws.services.ec2.model.Reservation;
 
+import com.amazonaws.services.ec2.model.DescribeAvailabilityZonesResult;
+import com.amazonaws.services.ec2.model.AvailabilityZone;
+
 import com.amazonaws.services.ec2.model.StartInstancesRequest;	// start instance에 필요한 라이브러리
+
+import com.amazonaws.services.ec2.model.DescribeRegionsResult;
+import com.amazonaws.services.ec2.model.Region;
+
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+
+import com.amazonaws.services.ec2.model.RunInstancesRequest;
+import com.amazonaws.services.ec2.model.RunInstancesResult;
 
 public class CC {
 	
@@ -67,16 +83,29 @@ public class CC {
 				listInstances();
 				break;
 			case 2:
+				AvailableZones();
+				break;
+			case 3:
 				StartInstance();
 				break;
-				
+			case 4:
+				AvailableRegions();
+				break;
+			case 5:
+				StopInstance();
+				break;
+			case 6:
+				CreateInstance();
+				break;
+			case 7:
+				RebootInstance();
+				break;
 			case 99:
 				System.out.print("Bye~.");
 				loop_tag = false;
 				break;
 			}
 		}
-			menu.close();
 		}
 		
 		public static void listInstances() {
@@ -106,12 +135,25 @@ public class CC {
 					System.out.println();
 				}
 				request.setNextToken(response.getNextToken());
-				
 				if(response.getNextToken() == null)
 				{
 					done = true;
 				}
 			}
+		}
+		
+		public static void AvailableZones() {
+			
+			System.out.println("Available zones....");
+	        
+	        DescribeAvailabilityZonesResult zones_response = ec2.describeAvailabilityZones();
+	        
+	        for(AvailabilityZone zone : zones_response.getAvailabilityZones()) {
+	        	System.out.printf( "[id] %s, " + "[region] %10s " + "[zone] %10s", zone.getZoneId(), zone.getRegionName(), zone.getZoneName());
+	        	System.out.println();
+	        }
+	        
+	        System.out.printf("\nYou have access to %d Availbility Zones.\n", zones_response.getAvailabilityZones().size());
 		}
 		
 		public static void StartInstance() {
@@ -127,8 +169,68 @@ public class CC {
 			StartInstancesRequest startInstancesRequest = new StartInstancesRequest().withInstanceIds(instance_id);
 			 
 	        ec2.startInstances(startInstancesRequest);
+		}
+		
+		public static void AvailableRegions() {
+			
+			System.out.println("Available Regions....");
+	        // 계정에 사용할 수 있는 리전을 나열하려면 AmazonEC2Client의 describeRegions 메서드를 호출
+	        DescribeRegionsResult regions_response = ec2.describeRegions();
 	        
-	        input.close();
+	        for(Region region : regions_response.getRegions()) {
+	        	System.out.printf( "[region] %20s " + ", [endpoint] %s", region.getRegionName(), region.getEndpoint());
+	        	System.out.println();
+	        }
+		}
+		
+		public static void StopInstance() {
+			
+			System.out.println("Stopping instance....");
+			
+			System.out.print("Enter an instance ID: ");
+			
+			Scanner input = new Scanner(System.in);
+			
+			String instance_id = input.next();
+			
+			StopInstancesRequest stopInstancesRequest = new StopInstancesRequest().withInstanceIds(instance_id);
+
+	        ec2.stopInstances(stopInstancesRequest).getStoppingInstances().get(0).getPreviousState().getName();
+		}
+		
+		public static void CreateInstance() {
+			
+			System.out.println("Create instance....");
+			
+			RunInstancesRequest runInstancesRequest = new RunInstancesRequest();
+			
+			runInstancesRequest.withImageId("ami-0b7554904b554fb53")
+							   .withInstanceType("t2.micro")
+			                   .withMinCount(1)
+			                   .withMaxCount(1)
+			                   .withKeyName("awskey01")
+			                   .withSecurityGroups("rules_01");
+
+			RunInstancesResult result = ec2.runInstances(runInstancesRequest);
+			
+			Instance instance = result.getReservation().getInstances().get(0);
+			String instance_id = instance.getInstanceId();
+			
+			System.out.printf("Create instance : %s \n", instance_id);
+		}
+		
+		public static void RebootInstance() {
+			
+			System.out.println("Stopping instance....");
+			
+			System.out.print("Enter an instance ID: ");
+			
+			Scanner input = new Scanner(System.in);
+			
+			String instance_id = input.next();
+			
+			RebootInstancesRequest rebootInstancesRequest = new RebootInstancesRequest().withInstanceIds(instance_id);
+			ec2.rebootInstances(rebootInstancesRequest);
 		}
 }
 
